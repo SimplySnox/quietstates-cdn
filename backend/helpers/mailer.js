954baa934a42e315f3f54baccff6225cebe7a766
snox <103229161+SimplySnox@users.noreply.github.com>
2026-03-25 01:44:53 -0400

    Implement email sending for login notifications

---
+++ backend/helpers/mailer.js
@@ -0,0 +1,28 @@
+import { Resend } from "resend";
+const resend = new Resend(process.env.RESEND_KEY);
+
+export default async function sendLoginEmail(toEmail, username) {
+    try {
+        await resend.emails.send({
+            from: "no-reply@simplysnox.com",
+            to: toEmail,
+            subject: "CDN Authentication Successful",
+            html: `
+                <div style="font-family:Arial;font-size:15px;">
+                    <h2>🎉 Authentication Successful</h2>
+                    <p>Hey <b>${username}</b>,</p>
+                    <p>You successfully authenticated with the <b>QS CDN / SimplySnox CDN</b>.</p>
+                    
+                    <p>If this wasn’t you, please reset your Discord password.</p>
+
+                    <br>
+                    <p>— QS CDN System</p>
+                </div>
+            `
+        });
+
+        console.log(`Sent login email to ${toEmail}`);
+    } catch (err) {
+        console.error("Email error:", err);
+    }
+}
