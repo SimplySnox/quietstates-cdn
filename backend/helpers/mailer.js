f81884b65be0e90f238bb0206e8314b900e0b8c2
snox <103229161+SimplySnox@users.noreply.github.com>
2026-03-25 02:12:22 -0400

    full update

--- backend/helpers/mailer.js
+++ backend/helpers/mailer.js
@@ -4,8 +4,8 @@ const resend = new Resend(process.env.RESEND_KEY);
 export default async function sendLoginEmail(toEmail, username) {
     try {
         await resend.emails.send({
-            from: "no-reply@simplysnox.com",
-            to: toEmail,
+            from: "Snox <no-reply@simplysnox.com>",
+            to: [toEmail ],
             subject: "CDN Authentication Successful",
             html: `
                 <div style="font-family:Arial;font-size:15px;">
@@ -13,7 +13,7 @@ export default async function sendLoginEmail(toEmail, username) {
                     <p>Hey <b>${username}</b>,</p>
                     <p>You successfully authenticated with the <b>QS CDN / SimplySnox CDN</b>.</p>
                     
-                    <p>If this wasn’t you, please reset your Discord password.</p>
+                    <p>If this wasn't you, please reset your Discord password.</p>
 
                     <br>
                     <p>— QS CDN System</p>
